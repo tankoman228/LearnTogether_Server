60cdfdff67db090bfdb26524060168da9a0becff
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2023-11-11 14:09:42 +0700

    Готовлю почву для создания API

--- DB_Objects/Account.py
+++ DB_Objects/Account.py
@@ -1,2 +1,21 @@
 class Account:
-    pass
+
+    def __init__(self, username, password, recovery_contact,
+                 title, icon, about, is_admin, admin_level,
+                 rating, last_seen):
+        self.username = username
+        self.password = password
+        self.recovery_contact = recovery_contact
+        self.title = title
+        self.icon = icon
+        self.about = about
+        self.is_admin = is_admin
+        self.admin_level = admin_level
+        self.rating = rating
+        self.last_seen = last_seen
+
+    def save_in_db(self):
+        pass
+
+    def delete_from_db(self):
+        pass
--- Sessions/Session.py
+++ Sessions/Session.py
@@ -1,2 +1,19 @@
+from DB_Objects.Account import Account
+from threading import Thread
+
+
 class Session:
-    pass
+
+    def __init__(self):
+        self.queue = []
+        self.started = False
+
+    def __cycle_new_thread(self):
+        pass
+
+    def cycle_begin(self):
+        self.started = True
+
+        t = Thread(target=self.__cycle_new_thread, args=())
+        t.daemon = True
+        t.start()
