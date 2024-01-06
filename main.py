226e8d0e638ef02ea53df9e72f5e33ce1bed1e16
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-01-06 14:04:13 +0700

    Решил всё переделать с нуля

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from sqlalchemy import text
+
 from ServerConsole import console_executor_start
 
+import DB
+DB.connect()
 
+accs = DB.query(text('SELECT * FROM Account'))
+
+for acc in accs:
+    print(acc.Username)
 
 console_executor_start()
 
