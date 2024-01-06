f9bdcaaa4c12426977720961873737f40f0b5b33
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-01-06 23:07:31 +0700

    Занялся обратной связью для пакета DB

--- DB/__init__.py
+++ DB/__init__.py
@@ -4,8 +4,6 @@ from sqlalchemy import create_engine, Executable, Table, text
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 
-import DB
-
 #  Session for work with database
 SessionEngine: Session
 
@@ -34,7 +32,12 @@ Comment: None
 Complaint: None
 
 
+#Connects to DBMS and trying to connect to DBMS
 def connect():
+
+    print('connection attempt start')
+
+    print('connection attempt ', 'config.json parsing')
     try:
         with open('config.json', 'r') as file:
             config = json.load(file)
@@ -50,6 +53,7 @@ def connect():
     except Exception as e:
         raise Exception("ERROR: config.json : data is not valid")
 
+    print('connection attempt ', 'db engine creating, searching for database')
     try:
         engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}")
 
@@ -86,38 +90,64 @@ def connect():
         Complaint = base.classes.complaint
 
     except Exception as e:
+
+        print('connection attempt ', "VALID DATABASE NOT FOUND, you can create it with script"
+              " CreateDB.sql or command recreate_db \n\n\n Error: ", e)
+
         engine = create_engine(f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}")
 
-        print("VALID DATABASE NOT FOUND, you can create it with script"
-              " CreateDB.sql or command recreate_db \n\n\n Error: ", e)
+        print('connection attempt ', 'Engine created with no connection to database')
 
     #
     global SessionEngine
     SessionEngine = Session(engine)
 
 
+#  Executes query and commits changes
 def query_and_commit(sql: Executable):
     r = SessionEngine.execute(sql)
     SessionEngine.commit()
     return r
 
 
+#  DROP IF EXISTS script start
 def recreate_db(args):
+
+    print('RECREATING DB def START')
+
+    script_file_path = 'DB\\DB_Queries\\CreateDB.sql'
+    if len(args) > 0:
+        script_file_path = ''
+        for arg in args:
+            script_file_path += arg + ' '
+        print('trying to load db from other script. Path to new script is: \n', script_file_path)
 
     if input('ATTENTION! This command will run script DROP IF EXISTS that means your \n'
              'previous database will be erased, this script will create new one using \n'
-             'script DB\\DB_Queries\\CreateDB.sql  print  1  to confirm ') != '1':
+             f'script | {script_file_path} | Print  1  to confirm ') != '1':
+        print('DROP IF EXISTS script execution cancelled. Existing database wouldn\'t be damaged')
         return
 
     #  recreate script start
-    f = open('DB\\DB_Queries\\CreateDB.sql', 'r')
+    try:
+        f = open(script_file_path, 'r')
+        sqls = f.read().split(';')
+        f.close()
 
-    sqls = f.read().split(';')
-    sqls.remove('')
-    for sql in sqls:
-        query_and_commit(text(sql))
+        try:
+            sqls.remove('')
+        except:
+            pass
+    except Exception as e:
+        print(f'Can\'t recreate database from script: {script_file_path}. '
+              f'Maybe, this file does not exists. \nError: {e}')
+        return
 
-    f.close()
+    try:
+        for sql in sqls:
+            query_and_commit(text(sql))
+    except Exception as e:
+        print('Your SQL script contained some errors, try recreate_db using right script. Error is: \n', e)
 
     #
     SessionEngine.close()
--- main.py
+++ main.py
@@ -5,10 +5,7 @@ from ServerConsole import console_executor_start
 import DB
 DB.connect()
 
-#accs = DB.SessionEngine.query(DB.Account).where(DB.Account.Username != 1)
 
-#for acc in accs:
-#    print(acc.Username)
 
 console_executor_start()
 
