826fe2e17d792cdeaa798e8fb4db17e3f0adcd5b
Tank 228 <92095847+tankoman228@users.noreply.github.com>
2024-03-09 19:49:12 +0700

    roles (http)

--- API/moderator.py
+++ API/moderator.py
@@ -9,7 +9,6 @@ app = FastAPI()
 
 @app.post('/get_roles')
 def fef(payload: dict = Body(...)):
-
     if str(payload['session_token']) not in AuthSession.auth_sessions.keys():
         return {"Error": 'Unregistered'}
 
@@ -76,7 +75,6 @@ def fef(payload: dict = Body(...)):
 
 @app.post('/delete_ib')
 def fef(payload: dict = Body(...)):
-
     session: AuthSession.AuthSession = AuthSession.auth_sessions[payload['session_token']]
 
     id_ib = int(payload["ID_InfoBase"])
@@ -97,23 +95,185 @@ def fef(payload: dict = Body(...)):
 
 
 @app.post('/change_user_role')
-def fef(payload: dict = Body(...)):
-    pass
+def change_user_role(payload: dict = Body(...)):
+    session: AuthSession.AuthSession = AuthSession.auth_sessions[payload['session_token']]
+
+    id_group = int(payload["ID_Group"])
+    id_role = int(payload["ID_Role"])
+    id_account = int(payload["ID_Account"])
+
+    sender_role = session.group_roles_cache[id_group]
+    target_role = DB.Ses.query(DB.Role).where(DB.Role.ID_Role == id_role).first()
+
+    if not session.allowed('edit_roles', id_group):
+        return {"Error": "Forbidden"}
+
+    try:
+
+        if not sender_role.IsAdmin and target_role.IsAdmin:
+            return {"Error": "User can't be admin because you are not"}
+
+        if sender_role.IsAdmin and target_role.IsAdmin and target_role.AdminLevel > sender_role.AdminLevel:
+            return {"Error": "User can't be made higher than you"}
+
+        for permission in target_role.permissions:
+            if permission not in sender_role.permissions:
+                return {"Error": "User can't have permissions that you don't have now"}
+
+        ag = (DB.Ses.query(DB.AccountGroup).where
+              (DB.AccountGroup.ID_Account == id_account and DB.AccountGroup.ID_Group == id_group).first())
+
+        ag.ID_Role = target_role.ID_Role
+        DB.Ses.commit()
+
+        return {"Success": True}
+
+    except Exception as e:
+
+        print('server error: ', e)
+        DB.Ses.rollback()
+        return {"Error": "Error"}
 
 
 @app.post('/new_role')
 def fef(payload: dict = Body(...)):
-    pass
+    session: AuthSession.AuthSession = AuthSession.auth_sessions[payload['session_token']]
+
+    id_group = int(payload["ID_Group"])
+    permissions: [] = payload['Permissions']
+
+    for permission in permissions:
+        if permission not in ['moderate_publications', 'offer_publications', 'edit_roles', 'edit_group',
+                              'forum_allowed', 'comments_allowed', 'moderate_comments',
+                              'create_tokens', 'ban_accounts']:
+            return {"Error": "Unknown permission"}
+
+    if not session.allowed('edit_roles', id_group):
+        return {"Error": "Forbidden"}
+
+    name = payload["Name"]
+    is_admin = bool(payload["IsAdmin"])
+    admin_level = int(payload["AdminLevel"])
+    sender_role = session.group_roles_cache[id_group]
+
+    if is_admin and not sender_role.IsAdmin:
+        return {"Error": "User can't be admin because you are not"}
+
+    if sender_role.IsAdmin and is_admin and admin_level > sender_role.AdminLevel:
+        return {"Error": "User can't be made higher than you"}
+
+    try:
+
+        role = DB.Role(
+            Name=name,
+            IsAdmin=is_admin,
+            AdminLevel=admin_level
+        )
+        DB.Ses.add(role)
+        DB.Ses.commit()
+
+        for permission in permissions:
+            DB.Ses.add(DB.Permission(
+                ID_Role=role.ID_Role,
+                Name=permission
+            ))
+            DB.Ses.commit()
+
+    except Exception as e:
+
+        print('server error: ', e)
+        DB.Ses.rollback()
+        return {"Error": "Error"}
 
 
 @app.post('/edit_role')
 def fef(payload: dict = Body(...)):
-    pass
+    session: AuthSession.AuthSession = AuthSession.auth_sessions[payload['session_token']]
+
+    id_role = int(payload["ID_Role"])
+    id_group = int(payload["ID_Group"])
+    permissions: [] = payload['Permissions']
+    role = DB.Ses.query(DB.Role).where(DB.Role.ID_Role == id_role).first()
+
+    for permission in permissions:
+        if permission not in ['moderate_publications', 'offer_publications', 'edit_roles', 'edit_group',
+                              'forum_allowed', 'comments_allowed', 'moderate_comments',
+                              'create_tokens', 'ban_accounts']:
+            return {"Error": "Unknown permission"}
+
+    if not session.allowed('edit_roles', id_group):
+        return {"Error": "Forbidden"}
+
+    if not DB.Ses.query(DB.AccountGroup).where(
+            DB.AccountGroup.ID_Role == id_role and DB.AccountGroup.ID_Group == id_group).first():
+        return {"Error": 469}
+
+    if role.IsAdmin and not session.group_roles_cache[id_group].IsAdmin:
+        return {"Error": "Forbidden in this case"}
+
+    if role.IsAdmin and role.AdminLevel > session.group_roles_cache[id_group].AdminLevel:
+        return {"Error": "Forbidden in this case :("}
+
+    name = payload["Name"]
+    is_admin = bool(payload["IsAdmin"])
+    admin_level = int(payload["AdminLevel"])
+    sender_role = session.group_roles_cache[id_group]
+
+    if is_admin and not sender_role.IsAdmin:
+        return {"Error": "User can't be admin because you are not"}
+
+    if sender_role.IsAdmin and is_admin and admin_level > sender_role.AdminLevel:
+        return {"Error": "User can't be made higher than you"}
+
+    try:
+
+        role.Name = name
+        DB.Ses.delete(DB.Ses.query(DB.Permission).where(DB.Permission.ID_Role == id_role).all())
+
+        for permission in permissions:
+            DB.Ses.add(DB.Permission(
+                ID_Role=role.ID_Role,
+                Name=permission
+            ))
+        DB.Ses.commit()
+
+    except Exception as e:
+
+        print('server error: ', e)
+        DB.Ses.rollback()
+        return {"Error": "Error"}
 
 
 @app.post('/delete_role')
 def fef(payload: dict = Body(...)):
-    pass
+    session: AuthSession.AuthSession = AuthSession.auth_sessions[payload['session_token']]
+
+    id_role = int(payload["ID_Role"])
+    id_group = int(payload["ID_Group"])
+
+    role = DB.Ses.query(DB.Role).where(DB.Role.ID_Role == id_role).first()
+
+    if not session.allowed('edit_roles', id_group):
+        return {"Error": "Forbidden"}
+
+    if not DB.Ses.query(DB.AccountGroup).where(
+            DB.AccountGroup.ID_Role == id_role).first():
+        return {"Error": 469}
+
+    if role.IsAdmin and not session.group_roles_cache[id_group].IsAdmin:
+        return {"Error": "Forbidden in this case"}
+
+    if role.IsAdmin and role.AdminLevel > session.group_roles_cache[id_group].AdminLevel:
+        return {"Error": "Forbidden in this case :("}
+
+    try:
+        DB.Ses.delete(role)
+        DB.Ses.commit()
+    except Exception as e:
+
+        print('server error: ', e)
+        DB.Ses.rollback()
+        return {"Error": "Error"}
 
 
 @app.post('/block_account')
@@ -177,7 +337,6 @@ def fef(payload: dict = Body(...)):
 
 @app.post('/get_complaints')
 def fef(payload: dict = Body(...)):
-
     session: AuthSession.AuthSession = AuthSession.auth_sessions[payload['session_token']]
     id_group = int(payload["ID_Group"])
 
